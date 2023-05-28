d589173443d18fb41c1b9b2b3db7e3d643404e94
qraxiss <qraxiss@gmail.com>
2023-05-28 15:00:10 +0300

    error status not send in body

--- api/middlewares/handle.ts
+++ api/middlewares/handle.ts
@@ -2,7 +2,17 @@ import { BaseError } from "../../errors/errors";
 import { toJson } from "../../errors/json";
 
 export function status500 (err: BaseError | Error, req: any, res: any, next: any) {
-  const res_json = toJson(err);
-  res.status(res_json.status);
-  res.json(res_json);
+  const json = toJson(err);
+
+  if (err instanceof BaseError){
+    res.status(json.status);
+    delete json.status
+  }
+
+  if (err instanceof Error){
+    res.status(500)
+  }
+
+
+  res.json(json);
 }

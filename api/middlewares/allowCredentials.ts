b12981995052f7f3b6a221da8431f3ba450d4662
qraxiss <qraxiss@gmail.com>
2023-05-28 15:23:47 +0300

    throwing errors formatted

--- api/middlewares/allowCredentials.ts
+++ api/middlewares/allowCredentials.ts
@@ -1,11 +1,11 @@
+import { ValidationError } from "../../errors/errors"
+
 export function allowCredentials (req: any, res: any, next: any) {
   //Check for user role
   try {
-    res.header('Access-Control-Allow-Credentials', 'true')
-
     return next()
   } catch (ex) {
-    if (ex instanceof Error) {
+    if (ex instanceof ValidationError) {
       req.session.user = undefined
     }
 
--- api/middlewares/requireAccess.ts
+++ api/middlewares/requireAccess.ts
@@ -1,10 +1,11 @@
 import { hasAccess } from '../../logic/models/user'
+import { ForbiddenError } from '../../errors/errors'
 
 export default (accessName: string) => {
   return (req:any, res: any, next: any) => {
     //Check for session and user role
     if (!req?.session?.user || hasAccess(accessName, req.session.user)){
-        return next(Error())
+        return next(new ForbiddenError())
     }
     return next()
   }

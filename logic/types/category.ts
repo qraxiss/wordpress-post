a0c62a1c096d576652ac06bceabb9252a8f72ddf
qraxiss <qraxiss@gmail.com>
2023-05-29 11:44:50 +0300

    category validators & type

---
+++ logic/types/category.ts
@@ -0,0 +1,9 @@
+type category = {
+  tags?: string[];
+  category: string;
+  img?: string;
+  category_id?: string;
+  _id?: string;
+};
+
+export type { category };
---
+++ logic/validators/category.ts
@@ -0,0 +1,28 @@
+import * as Joi from 'joi';
+import { joiObjectId } from 'ts-joi-objectid';
+
+const objectId = joiObjectId(Joi);
+
+
+export const create = Joi.object({
+    tags: Joi.array().items(Joi.string()),
+    category: Joi.string().required(),
+    img: Joi.string()
+})
+
+export const update = Joi.object({
+    category_id: objectId().required(),
+
+    category: Joi.string(),
+    tags: Joi.array().items(Joi.string()),
+    img: Joi.string()
+}).or('tags', 'category', 'img')
+
+export const remove = Joi.object({
+    category_id: objectId().required()
+})
+
+export const get = Joi.object({
+    category_id: objectId(),
+    category: Joi.string(),
+}).nand('category_id', 'category')

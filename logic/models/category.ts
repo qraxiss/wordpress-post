b5cb7be00f7b97b04daf17c64d43285068a1979f
qraxiss <qraxiss@gmail.com>
2023-05-29 11:45:17 +0300

    template models of category

---
+++ logic/models/category.ts
@@ -0,0 +1,22 @@
+import { category as categoryModel } from "../../database/models";
+import { category as categoryType } from "../types/category";
+import * as validators from "../validators/category";
+import { validate } from "../helpers/validator";
+
+import { filter, rename } from "../helpers/filter";
+
+export async function create(params: object) {
+    return {}
+}
+
+export async function get(params: object) {
+    return {}
+}
+
+export async function update(params: object) {
+    return {}
+}
+
+export async function remove(params: object) {
+    return {}
+}

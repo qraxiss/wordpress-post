4efd578010c53f851e8d27d1ad63db7be0e4184a
qraxiss <qraxiss@gmail.com>
2023-05-31 09:22:16 +0300

    name change wp->wordpress

---
+++ logic/helpers/wordpress.ts
@@ -0,0 +1,12 @@
+import axios from 'axios'
+import config from '../../config'
+
+const basic: string = `Basic ${Buffer.from(`${config.USERNAME}:${config.PASSWORD}`, 'utf-8').toString('base64')}`
+
+export const api = axios.create({
+    baseURL: config.API_GATE,
+    headers: {
+        'Content-Type': 'application/json',
+        Authorization: basic
+    }
+})

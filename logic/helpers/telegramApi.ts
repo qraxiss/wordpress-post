724908ef580c00a69556dd4f60289362b780cfc9
qraxiss <qraxiss@gmail.com>
2023-06-04 10:04:32 +0300

    telegram helper

--- logic/helpers/telegramApi.ts
+++ logic/helpers/telegramApi.ts
@@ -4,6 +4,13 @@ import config from '../../config'
 export const api = axios.create({
     baseURL: config.TELEGRAM_API,
     headers: {
+        Authorization: `Bearer 123`,
         'Content-Type': 'application/json'
     }
 })
+
+export async function sendMessage(message: string) {
+    return api.post('send-message', {
+        text: message
+    })
+}

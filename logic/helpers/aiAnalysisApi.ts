a01097c4c3a87618781f92f5ae10e809c6f0d414
qraxiss <qraxiss@gmail.com>
2023-06-04 10:04:15 +0300

    ai-analysis api helper

--- logic/helpers/aiAnalysisApi.ts
+++ logic/helpers/aiAnalysisApi.ts
@@ -1,9 +1,19 @@
 import axios from 'axios'
 import config from '../../config'
 
+import { openai_question, openai_response } from '../types/aiAnalysis'
+
 export const api = axios.create({
     baseURL: config.AI_ANALYSIS_API,
     headers: {
         'Content-Type': 'application/json'
     }
 })
+
+export async function getAiResponse(report: openai_question): Promise<openai_response> {
+    const response = await api.get('/openai', {
+        data: report
+    })
+
+    return response.data as openai_response
+}
